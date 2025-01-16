660f75df695bf425f03bb40a7f73264d45343aa6
AathiKrishna-JR <aathi3612@gmail.com>
2025-01-16 12:58:48 +0530

    place 1.2

--- src/app/Place/place.component.ts
+++ src/app/Place/place.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component ({
     selector: 'app-place',
     standalone: true,
-    templateUrl:'./user.component.html',
-    styleUrl : './user.component.css',
+    templateUrl:'./place.component.html',
+    styleUrl : './place.component.css',
 })
 export class PlaceComponent {}
