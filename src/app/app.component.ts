3db1c3cdc11f7383847b8499e602428008174a8b
AathiKrishna-JR <aathi3612@gmail.com>
2025-01-16 12:56:55 +0530

    place1.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {HelloWorldComponent} from './World/hello-world.component';
+import { PlaceComponent } from './Place/place.component';
 
 @Component({
 selector: 'app-root',
 standalone: true,
-imports : [ HelloWorldComponent],
+imports : [ HelloWorldComponent,PlaceComponent],
 templateUrl: './app.component.html',
 styleUrl: './app.component.css',
 })
