6ff2dac3dda891d7badc5ce74bb15da2f6633e9c
AathiKrishna-JR <aathi3612@gmail.com>
2025-01-16 23:56:44 +0530

    7wonder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {HelloWorldComponent} from './World/hello-world.component';
 import { PlaceComponent } from './Place/place.component';
+import { DetailComponent } from './Details/details.component';
 import { w7 } from './Place/w7';
 
 @Component({
 selector: 'app-root',
 standalone: true,
-imports : [ HelloWorldComponent,PlaceComponent],
+imports : [ HelloWorldComponent,PlaceComponent,DetailComponent],
 templateUrl: './app.component.html',
 styleUrl: './app.component.css',
 })
