913834750f19bcd85abbe47472d218aefdc1c103
AathiKrishna-JR <aathi3612@gmail.com>
2025-01-17 17:15:57 +0530

    new 3

--- src/app/Card/card.component.ts
+++ src/app/Card/card.component.ts
@@ -2,6 +2,11 @@ import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { FormsModule, NgForm} from "@angular/forms";
 
+ 
+let username: string[] = ["aathi","krishna"];
+let password: string[] = ["12345678","87654321"];
+    
+
 @Component({
     selector: 'app-card',
     standalone : true,
@@ -12,13 +17,31 @@ import { FormsModule, NgForm} from "@angular/forms";
 
 
 export class CardComponent {
-
-    onLogin(login : NgForm)
+    
+   
+     onLogin(login : NgForm)
     {
-        console.log(login);
+        if( login.value['email'].length == 0 || login.value['password'].length == 0)
+        {
+          alert("Enter value in both field");
+        } 
+
+        else if( login.value['password'].length < 8)
+        {
+          alert("Enter password for atleast 8 letters");
+        }
+        let index = username.indexOf(login.value['email']);
+        let pass = password[index];
+        if (pass === login.value['password']) {
+            alert(login.value['email']+" sucessfully logined");
+        } else {
+            alert("Invalid credentials");
+        }
+    }
         
     }
+
     
-}
+
 
 export class CardOverviewExample {}
